7627ddb3f777be30e4493f5247b5af1a246089d3
jrpelegrina <juapesai@hotmail.com>
2026-03-20 10:49:08 +0100

    Added timeout when check connection with server

--- install-files/usr/share/n4d/python-plugins/ShutdownerManager.py
+++ install-files/usr/share/n4d/python-plugins/ShutdownerManager.py
@@ -6,8 +6,7 @@ import subprocess
 import copy
 import time
 import n4d.server.core as n4dcore
-import n4d.responses
-import xmlrpc.client as n4dclient
+import n4d.client
 import ssl
 import re
 
@@ -400,9 +399,8 @@ class ShutdownerManager:
 	def _check_connection_with_server(self):
 
 		try:
-			context=ssl._create_unverified_context()
-			client=n4dclient.ServerProxy('https://server:9779',context=context,allow_none=True)
-			test=client.is_cron_enabled('','ShutdownerManager')
+			client=n4d.client.Client('https://server:9779',timeout=10)
+			test=client.ShutdownerManager.is_cron_enabled()
 			self.is_clientized_desktop=False
 			return True
 		except Exception as e:
